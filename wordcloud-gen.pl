d7a2bafceface9e013cf2525782410a10d5385cb
ice1000 <ice1000kotlin@foxmail.com>
2019-11-23 06:19:57 -0500

    [ wc ] Update word-cloud generator

--- wordcloud-gen.pl
+++ wordcloud-gen.pl
@@ -7,16 +7,27 @@ use warnings FATAL => 'all';
 my $mds = `cat *.lagda *.tex`;
 
 $mds =~ s/\\begin\{.*\}//g;
+$mds =~ s/\\end\{.*\}//g;
 $mds =~ s/\\[a-zA-Z0-9]+//g;
+
+my $arg = "--width 1280 --height 720 --background white --color blue";
 
 my $tmp = 'wordcloud-tmp';
 open(my $fh, '>', $tmp) or die "Failed to open file '$tmp', $!";
 print $fh $mds;
 close $fh;
 
-my $arg = "--width 1280 --height 720 --background white --color blue";
-
 print `cat $tmp | wordcloud_cli $arg --imagefile pic-wordcloud.png`;
 print `rm $tmp`;
 
+$mds =~ s/type//gi;
+$mds =~ s/coe//gi;
+$mds =~ s/path//gi;
+
+open($fh, '>', $tmp) or die "Failed to open file '$tmp', $!";
+print $fh $mds;
+close $fh;
+
+print `cat $tmp | wordcloud_cli $arg --imagefile pic-wordcloud-no-coe-path-type.png`;
+print `rm $tmp`;
 print "Generation done.\n";
